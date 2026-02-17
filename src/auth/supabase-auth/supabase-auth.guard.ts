eb8ffc5590799700f7e554a0a332bd7d198ac5f8
shaharyar <shaharyar4t4@gmail.com>
2026-02-17 16:30:32 +0500

    update some issue

--- src/auth/supabase-auth/supabase-auth.guard.ts
+++ src/auth/supabase-auth/supabase-auth.guard.ts
@@ -1,34 +1,69 @@
+// import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+// import { Observable } from 'rxjs';
+// import * as jwt from 'jsonwebtoken';
+// import {Request} from 'express';
+// import { ConfigService } from '@nestjs/config';
+
+// @Injectable()
+// export class SupabaseAuthGuard implements CanActivate {
+//   constructor(private configServices: ConfigService){}
+//   canActivate(
+//     context: ExecutionContext,
+//   ): boolean | Promise<boolean> | Observable<boolean> {
+
+//     const request = context.switchToHttp().getRequest<Request>();
+//     // this below line show that the authorization code share to header files
+//     const authHeader = request.headers['authorization'];
+//     // bearer means holder the token part
+//     if(!authHeader || !authHeader.startsWith('Bearer ')){
+//       throw new UnauthorizedException('No Token Provided!');
+//     }
+//     const token = authHeader.split(' ')[1];
+//     const jwtKey = this.configServices.get<string>('SUPABASE_JWT_SECRET');
+//     if(!jwtKey){
+//       throw new UnauthorizedException('Jwt key is not found');
+//     }
+//     try{
+//       const decode = jwt.verify(token, jwtKey);
+//       request['user'] =decode;
+//       return true;
+//     }catch(error){
+//       throw new UnauthorizedException('Invalid Token!');
+//     }
+    
+//   }
+// }
+
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import * as jwt from 'jsonwebtoken';
-import {Request} from 'express';
+import { Request } from 'express';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SupabaseAuthGuard implements CanActivate {
-  constructor(private configServices: ConfigService){}
+  constructor(private configService: ConfigService){}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-
     const request = context.switchToHttp().getRequest<Request>();
-    // this below line show that the authorization code share to header files
     const authHeader = request.headers['authorization'];
-    // bearer means holder the token part
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
+    if(!authHeader || !authHeader.startsWith('Bearer ')) {
       throw new UnauthorizedException('No Token Provided!');
     }
     const token = authHeader.split(' ')[1];
-    const jwtKey = this.configServices.get<string>('SUPABASE_JWT_SECRET');
-    if(!jwtKey){
-      throw new UnauthorizedException('Jwt key is not found');
+    const jwtSecret = this.configService.get<string>('SUPABASE_JWT_SECRET');
+    if(!jwtSecret){
+      throw new UnauthorizedException('JWT Secrete not found');
     }
     try{
-      const decode = jwt.verify(token, jwtKey);
-      request['user'] =decode;
+      const decode = jwt.verify(token, jwtSecret);
+      request['user'] = decode;
       return true;
-    }catch(error){
-      throw new UnauthorizedException('Invalid Token!');
+    }
+    catch(error) {
+      throw new UnauthorizedException('Invalid Token');
     }
     
   }
